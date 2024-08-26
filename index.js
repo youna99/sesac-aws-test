75712ce84281301e3cd6566a51b2ba6a4980a05b
youna99 <dbsk0108@gmail.com>
2024-08-26 15:36:07 +0900

    aws domo

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const app = express();
+const { Sequelize } = require('sequelize');
+const userModel = require('./models/User');
+const PORT = 8000;
+require('dotenv').config();
+
+// Sequelize 연결 설정
+const sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+        host: process.env.DB_HOST,
+        dialect: 'mysql',
+    }
+);
+// 모델 초기화
+const User = userModel(sequelize);
+
+// 미들웨어 설정
+app.use(express.json());
+app.get('/', (req, res) => {
+    res.send('안녕하세요!!');
+});
+
+app.post('/api/users', async (req, res) => {
+    try {
+        const { username, email } = req.body;
+        const user = await User.create({ username, email });
+        res.json(user);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ massage: '서버 에러' });
+    }
+});
+
+sequelize.sync({ force: false }).then(() => {
+    console.log('테이블 생성 완료!');
+
+    app.listen(PORT, () => {
+        console.log(`http://localhost:${PORT}`);
+    });
+});
